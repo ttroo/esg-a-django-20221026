0bf88198881d0eab315f2fd9a0be36b514211335
ttroo <kimttroo@gmail.com>
2022-10-28 16:18:48 +0900

    accounts 생성 수정 조회 만들기

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+
+
+login = LoginView.as_view(
+    template_name = "accounts/login_form.html",
+)
+
+
+
+logout = LogoutView.as_view(
+    next_page = "/accounts/login/",
+)
+
+
+@login_required
+# @goldmembership_required
+def profile(request):
+    return render(request, "accounts/profile.html")
+
+
+
+signup = CreateView.as_view(
+    from_class = UserCreationForm,
+    success_url="/accounts/login/",
+    template_name="accounts/signup_form.html",
+)
